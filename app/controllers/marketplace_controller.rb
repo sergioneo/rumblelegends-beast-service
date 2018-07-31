8a1e84f9145dfdeafb1a8570d1e68ee435061628
Sergio Yanez <sergioy@amazon.com>
2018-07-31 13:50:05 -0700

    Mock marketplace get

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -5,8 +5,16 @@ class MarketplaceController < ApplicationController
 
   	beasts = Array.new
 
+    example_beast = {
+      "name": "Pepito",
+      "other_attr": 3,
+      "likes": 23
+    }
+
+    beast_image = "https://image-generator-beta.herokuapp.com/dinosaur/590295810358705651712"
+
   	(1..number_per_page).each do |i|
-  		beast_card = (render_to_string partial: "templates/beast_card")
+  		beast_card = (render_to_string partial: "templates/beast_card", locals: {:beast => example_beast, :image_url => beast_image})
   		pricing_url = "SOME_URL"
   		beasts << {:card => beast_card, :pricing => pricing_url}
   	end
