b50a14aabc421d9c1510349642b161bfad4b2aba
Sergio Yanez <sergioy@amazon.com>
2018-08-02 07:55:11 -0700

    V1 ready

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -1,21 +1,24 @@
 class MarketplaceController < ApplicationController
   def get
 
+    require 'json'
+
   	number_per_page = 10
+
+    query_url = ENV["INFORMATION_SERVICE"]+'query'
+    query_result = JSON.parse(Typhoeus.get(query_url, followlocation: true).body)
+    image_base_url = ENV["IMAGE_GENERATION_SERVICE"]
+
+    beast_type_strings = {"0" => "dinosaur", "1" => "unicorn"}
 
   	beasts = Array.new
 
-    example_beast = {
-      "name": "Pepito",
-      "other_attr": 3,
-      "likes": 23
-    }
-
-    beast_image = "https://image-generator-beta.herokuapp.com/dinosaur/590295810358705651712"
-
-  	(1..number_per_page).each do |i|
-  		beast_card = (render_to_string partial: "templates/beast_card", locals: {:beast => example_beast, :image_url => beast_image})
-  		pricing_url = "SOME_URL"
+    query_result.each do |beast|
+      puts beast
+      beast_type_string = beast_type_strings[beast["beast_type"].to_s]
+      image_url = image_base_url+beast_type_string+"/"+beast["genes"]
+  		beast_card = (render_to_string partial: "templates/beast_card", locals: {:beast => beast, :image_url => image_url})
+  		pricing_url = ENV["PRICING_SERVICE"]+"beast/"+beast["external_id"].to_s
   		beasts << {:card => beast_card, :pricing => pricing_url}
   	end
 
