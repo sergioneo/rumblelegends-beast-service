47ca39e900d5264632585fd11b3fe42353d8fb0c
Sergio Yanez <sergioy@amazon.com>
2018-09-03 23:54:10 -0700

    Pagination

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -3,9 +3,36 @@ class MarketplaceController < ApplicationController
 
     require 'json'
 
-  	number_per_page = 10
+  	number_per_page = "10"
+    unless params["amountPerPage"].blank?
+      number_per_page = params["amountPerPage"]
+    end
 
-    query_url = ENV["INFORMATION_SERVICE"]+'query'
+    page = 1
+    unless params["page"].blank?
+      page = params["page"].to_i
+    end
+
+    offset = (page - 1) * number_per_page.to_i
+
+    start = "0"
+    unless params["start"].blank?
+      start = params["start"]
+    end
+
+    orderBy = "external_id"
+    unless params["orderBy"].blank?
+      orderBy = params["orderBy"]
+    end
+
+    asc = "ASC"
+    unless params["asc"].blank?
+      asc = params["asc"]
+    end
+
+
+    query_url = ENV["INFORMATION_SERVICE"]+'query?limit='+number_per_page+"&offset="+offset.to_s+"&start="+start+"&orderBy="+orderBy+"&asc="+asc
+    puts query_url
     query_result = JSON.parse(Typhoeus.get(query_url, followlocation: true).body)
     image_base_url = ENV["IMAGE_GENERATION_SERVICE"]
 
@@ -20,9 +47,10 @@ class MarketplaceController < ApplicationController
     query_result["hits"].each do |beast|
       beast_type_string = beast_type_strings[beast["beast_type"].to_s]
       image_url = image_base_url+beast_type_string+"/"+beast["genes"]
-  		beast_card = (render_to_string partial: "templates/beast_card", locals: {:beast => beast, :image_url => image_url})
   		pricing_url = ENV["PRICING_SERVICE"]+"beast/"+beast["external_id"].to_s
-  		beasts << {:card => beast_card, :pricing => pricing_url, :id => beast["external_id"]}
+      beast[:pricing] = pricing_url
+      beast[:image_url] = image_url
+  		beasts << beast
   	end
 
     return_payload["hits"] = beasts
