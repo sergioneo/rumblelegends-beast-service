77adab48cde72811434ae8fddddcb551a3b5882d
Sergio Yanez <sergioy@amazon.com>
2018-09-13 12:53:16 -0700

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'beast/:id', to: "detail#get_beast"
 
-  get 'marketplace/get_beasts', to: "marketplace#get"
+  get 'beast/marketplace', to: "marketplace#get"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
