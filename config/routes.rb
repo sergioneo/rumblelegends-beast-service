36b72433dc7a971fa1d16ab10bffa3c5024be18b
Sergio Yanez <sergioy@amazon.com>
2018-09-13 19:25:20 -0700

    Marketplace get

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'beast/:id', to: "detail#get_beast"
-
   get 'beast/marketplace', to: "marketplace#get"
+
+  get 'beast/:id', to: "detail#get_beast"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
