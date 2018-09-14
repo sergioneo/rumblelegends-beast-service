ba9d330d57d13eff4274910495ded3313d325547
Sergio Yanez <sergioy@amazon.com>
2018-09-13 19:22:04 -0700

    moving sire/matron call to inside if statement

--- app/controllers/detail_controller.rb
+++ app/controllers/detail_controller.rb
@@ -8,23 +8,20 @@ class DetailController < ApplicationController
   	beast = JSON.parse(Typhoeus.get(query_url, followlocation: true).body)
   	beast_type_string = beast_type_strings[beast["beast_type"].to_s]
   	image_url = image_base_url+beast_type_string+"/"+beast["genes"]
-	pricing_url = ENV["PRICING_SERVICE"]+"beast/"+beast["external_id"].to_s
+	  pricing_url = ENV["PRICING_SERVICE"]+"beast/"+beast["external_id"].to_s
   	beast[:pricing] = pricing_url
   	beast[:image_url] = image_url
 
   	query_url_sire = ENV["INFORMATION_SERVICE"]+'beast/'+beast["sireId"].to_s
   	query_url_matron = ENV["INFORMATION_SERVICE"]+'beast/'+beast["matronId"].to_s
 
-  	sire = JSON.parse(Typhoeus.get(query_url_sire, followlocation: true).body)
-  	matron = JSON.parse(Typhoeus.get(query_url_matron, followlocation: true).body)
-
-    if beast["generation"] != 0
-
+  	if beast["generation"] != 0
+      sire = JSON.parse(Typhoeus.get(query_url_sire, followlocation: true).body)
+      matron = JSON.parse(Typhoeus.get(query_url_matron, followlocation: true).body)
     	beast[:sire] = sire
     	beast[:sire][:image_url] = image_base_url+beast_type_string+"/"+sire["genes"].to_s
     	beast[:matron] = matron
     	beast[:matron][:image_url] = image_base_url+beast_type_string+"/"+sire["matron"].to_s
-
     else
       beast[:sire] = Hash.new
       beast[:matron] = Hash.new
